bf8a18d1063afb2e26e8972b5ce764056fcfc7c3
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-15 17:42:19 -0600

    Container: 404 Not Found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/components/NotFound.scss'
+
+const NotFound = () => (
+  <section className='notFound'>
+    <h1>No Encontrado</h1>
+    <h2>404</h2>
+    <a href>Ir a inicio</a>
+  </section>
+)
+
+export default NotFound
