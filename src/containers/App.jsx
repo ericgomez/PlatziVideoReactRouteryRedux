e30223d67423dbb021c3902aff0563771075276b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-15 12:14:11 -0600

    Estructura y Estilos del Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
+import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss'
 
@@ -17,9 +18,10 @@ const App = () => (
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
-        <CarouselItem />
       </Carousel>
     </Categories>
+
+    <Footer />
   </div>
 )
 
