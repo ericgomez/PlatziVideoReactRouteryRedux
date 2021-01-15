b6cd9adf01724c21d3e6d291750d5a2825ea3a2e
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-15 10:32:18 -0600

    Estructura del Header

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,52 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
-  resolve:{
-      extensions: ['.js', '.jsx']
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
-  module:{
-      rules:[
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
           {
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use:{
-                  loader: "babel-loader"
-              }
-          },
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
           {
-              test: /\.html$/,
-              use:[
-                  {
-                      loader: "html-loader"
-                  }
-              ]
+            loader: MiniCssExtractPlugin.loader
           },
-          {
-            test: /\.(s*)css$/,
-            use: [
-              { 
-                loader: MiniCssExtractPlugin.loader 
-              },
-                'css-loader',
-                'sass-loader',
-            ],
-          },
-      ]
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   },
-  plugins:[
-      new HtmlWebPackPlugin({
-          template: './public/index.html',
-          filename: './index.html'
-
-      }),
-      new MiniCssExtractPlugin({
-        filename: 'assets/[name].css',
-      }),
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css'
+    })
   ]
-};
+}
