02b0483620fd2f858c70e27af82e09b622e931ad
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-15 12:37:51 -0600

    Añadiendo imágenes con Webpack

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,9 +12,29 @@ const App = () => (
   <div className='App'>
     <Header />
     <Search />
-    <Categories>
+
+    <Categories title="Mi Lista">
       <Carousel>
         <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Tendencias">
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+    <Categories title="Originales de PlatziVideo">
+      <Carousel>
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
