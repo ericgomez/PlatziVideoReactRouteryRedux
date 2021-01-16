27c519c7f74ce3eb688021af322bf30bfff633fd
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-16 10:20:43 -0600

    Componente: Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
-import Footer from '../components/Footer'
 import useInitialState from '../hooks/useInitialState'
 
 import '../assets/styles/Home.scss'
@@ -16,8 +14,7 @@ const Home = () => {
   return initialState.length === 0 ? (
     <h1>Loading...</h1>
   ) : (
-    <div className='Home'>
-      <Header />
+    <>
       <Search />
       {initialState.mylist.length > 0 && ( //Creamos una validación mylist que los registros sean mayor a 0
         <Categories title='Mi Lista'>
@@ -50,9 +47,7 @@ const Home = () => {
           </Carousel>
         </Categories>
       )}
-
-      <Footer />
-    </div>
+    </>
   )
 }
 
