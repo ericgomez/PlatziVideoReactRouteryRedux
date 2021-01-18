fc4477e85328fd4f12d18fdc0910eec718706aab
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-18 11:22:33 -0600

    Uso de gravatar en nuestro proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,48 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import gravatar from '../utils/gravatar'
 import '../assets/styles/components/Header.scss'
 
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import userIcon from '../assets/static/user-icon.png'
 
-const Header = () => (
-  <header className='header'>
-    <Link to="/">
-      <img className='header__img' src={logo} alt='Platzi Video' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='User' />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <Link to='/register'>Cuenta</Link>
-        </li>
-        <li>
-          <Link to='/login'>Iniciar sesión</Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-)
+const Header = props => {
+  const { user } = props
+  const hasUser = Object.keys(user).length > 0 //Validacion para saber su user tiene elementos
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
 
-export default Header
+          {hasUser ? (//Si hasUSer es verdadero mandamos al imagen del Gravatar 
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (//en caso de que sea falso mandamos una imagen por defecto
+            <img src={userIcon} alt='User' />
+          )}
+
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <Link to='/register'>Cuenta</Link>
+          </li>
+          <li>
+            <Link to='/login'>Iniciar sesión</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(Header)
